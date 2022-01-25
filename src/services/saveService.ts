515591a6f2334b815b09bb61685f80101fb968b4
11mad11 <mad.21@live.fr>
2022-01-25 10:13:35 -0500

    added saving to db logic

--- src/services/saveService.ts
+++ src/services/saveService.ts
@@ -3,25 +3,78 @@ import {Schema, SchemaField} from "@/services/dataService";
 import {format} from 'sql-formatter';
 import {LogService} from "@/services/logService";
 import {TableConfig, TableConfigs, TablesDef} from "@/services/index";
+import {now} from "moment";
 
 const logger = LogService.logger({name: "SaveService"});
 
 export class SaveService<T extends TablesDef, C extends TableConfigs<T>> extends BaseService<T, C, any> {
     
-    public apply() {
+    public async apply(dry: boolean = true) {
         const sqls = [];
+        const mods = this.services.modification.mods;
         
-        logger.log(format(sqls.reduce((a, v) => {
-            return a + v + ";";
-        }, ""), {
-            linesBetweenQueries: 3,
-            uppercase: true
-        }));
+        for (const table in this._tables) {
+            const schema = this.services.data.getSchema(table as any).value;
+            const keys = Object.keys(schema).filter(k => schema[k].Key !== "PRI");
+            
+            const values = [];
+            
+            Object.entries(mods[table]).forEach(([id, val]) => {
+                if (parseInt(id) >= 0) {//Modification
+                    const vals = keys.map(k => {
+                        if (val[k] !== undefined || k === "Date_Modification")
+                            return `${k}=${quote(val[k], schema[k])}`;
+                    }).filter(s => !!s);
+                    sqls.push(`UPDATE ${table}
+					           SET ${vals.join(",")}
+					           WHERE
+						           ID = ${id}`);
+                } else {//Creation
+                    const vals = keys.map(k => quote(val[k], schema[k], true));
+                    if (!val["Prix_ID"])
+                        console.log(id, val);
+                    values.push(`(${vals.join(",")})`);
+                }
+            })
+            
+            if (values.length) {
+                let sql = `INSERT INTO ${table} (
+					${keys.join(",")}
+				                                )
+				           VALUES ${values.join(',')}`;
+                sqls.push(sql);
+            }
+        }
+    
+        if(!dry){
+            const conn = await this.services.data.conn.getConnection();
+            try {
+                await conn.beginTransaction();
+            
+                await conn.execute(sqls.reduce((a, v) => {
+                    return a + v + ";";
+                }));
+            
+                await conn.commit()
+            } catch (e) {
+                if (conn) await conn.rollback();
+                logger.error(e);
+            } finally {
+                if (conn) await conn.release();
+            }
+        }
+        
+        return sqls;
     }
     
 }
 
-function quote(v: any, def: SchemaField<any>) {
+function quote(v: any, def: SchemaField<any>, creation: boolean = false) {
+    if (def.Field === "Date" && (creation || v === undefined))
+        return Math.floor(now() / 1000);
+    if (def.Field === "Date_Modification")
+        return Math.floor(now() / 1000);
+    
     const type = def.Type.split("(")[0];
     switch (type) {
         case "bigint":
@@ -30,7 +83,8 @@ function quote(v: any, def: SchemaField<any>) {
         case "decimal":
             return v ? parseFloat(v) : def.Default;
         case "text":
-        default:
             return v ? `"${v?.toString()}"` : 'NULL';
+        default:
+            throw new Error("Field type not supported");
     }
 }
