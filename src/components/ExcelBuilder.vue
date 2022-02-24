3799abbc2cfc750cd102c09f0c1962cac120fea9
11mad11 <mad.21@live.fr>
2022-02-24 15:53:16 -0500

    updated ExcelBuilder.vue Price report

--- src/components/ExcelBuilder.vue
+++ src/components/ExcelBuilder.vue
@@ -11,47 +11,41 @@ import {StoreState} from "@/store";
 import {defineComponent} from "vue";
 import axios from "axios";
 import {useRouter} from "vue-router";
+import {MyTablesDef, useMyServices} from "@/config/dataConfig";
 
 export default defineComponent({
 	name: "ExcelBuilder",
 	setup() {
 		const store = useStore<StoreState>();
 		const router = useRouter();
+		const services = useMyServices();
 
 		return {
 			async btn_prix() {
-				const pricesIndex = Object.values(store.state._.priceTitles).reduce((a, v, i) => {
-					a[v.ID] = i;
-					return a;
-				}, {});
-				const pricesTitle = Object.values(store.state._.priceTitles).reduce((a, v, i) => {
-					a[i] = v.Titre;
-					return a;
-				}, []);
+				const pricesByID = services.data.tables.prix.value;
+				const pricesOrdered = Object.values(pricesByID).sort((a, b) => b.Position - a.Position);
 
-				const pricesByProduct = {};
-				for (const price of Object.values(store.state._.prices)) {
-					pricesByProduct[price.Produit_ID] = pricesByProduct[price.Produit_ID] || new Array(pricesTitle.length);
-					pricesByProduct[price.Produit_ID][pricesIndex[price.Prix_ID]] = price.Prix;
+				const pricesByProduct: Record<number, Record<number, MyTablesDef["produits_prix"]>> = {};
+				for (const price of Object.values(services.data.tables.produits_prix.value)) {
+					pricesByProduct[price.Produit_ID] = pricesByProduct[price.Produit_ID] || new Array(Object.keys(pricesByID).length);
+					pricesByProduct[price.Produit_ID][price.Prix_ID] = price;
 				}
 
 				const products = [];
-				Object.values(store.state._.products).forEach((p: any, i) => {
-					products[i] = [
+				Object.values(services.data.tables.produits.value).forEach((p) => {
+					products.push([
 						p.Code,
 						p.Variete,
-						p.Format
-					];
-					if (pricesByProduct[p.ID])
-						products[i].push(...pricesByProduct[p.ID]);
+						p.Format,
+						...pricesOrdered.map(v => pricesByProduct[p.ID]?.[v.ID]?.Prix)
+					]);
 				});
 
 				const workbook = new ExcelJS.Workbook();
 				const sheet = workbook.addWorksheet('My Sheet', {});
 
-				sheet.addRow(["Code", "Variete", "Format", ...pricesTitle]);
+				sheet.addRow(["Code", "Variete", "Format", ...pricesOrdered.map(v=>v.Titre)]);
 				sheet.addRows(products);
-
 
 				const buffer = await workbook.xlsx.writeBuffer();
 
@@ -63,24 +57,8 @@ export default defineComponent({
 				const response = await axios.post("http://plantselect.lithiumwebsolutions.com/php/temp_file_server.php", form, {
 					headers: {},
 				});
-				//console.log(response.data);
 
 				await router.push({name: "ExcelViewer", params: {file: response.data}})
-
-				/*//const buffer = await workbook.xlsx.writeBuffer();
-				const a = document.createElement("a"),
-					url = URL.createObjectURL(new Blob([buffer], {
-						type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-					}));
-				a.href = url;
-				a.download = "test.xlsx";
-				document.body.appendChild(a);
-				a.click();
-				setTimeout(function () {
-					document.body.removeChild(a);
-					window.URL.revokeObjectURL(url);
-				}, 0);*/
-
 			}
 		};
 	}
